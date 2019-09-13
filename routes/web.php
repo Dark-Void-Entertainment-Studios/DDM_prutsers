b8ce5118270c67939578c0456d4dd8c4b8982fb4
rens gouman <99030922@mydavinci.nl>
2019-09-13 14:26:50 +0200

    user blade made
    just the start

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user', 'UserController@index');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['adminCheck']], function(){
+    Route::get('/user', 'UserController@index');
+});
+
+/*
+Route::prefix('admin')->group(function () {
+    Route::get('users', function () {
+        // Matches The "/admin/users" URL
+    });
+});
