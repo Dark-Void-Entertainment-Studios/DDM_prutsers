9a2bc4689188b32c43521b7b44b0eafd31ebf585
rens gouman <99030922@mydavinci.nl>
2019-09-13 12:04:37 +0200

    mini commit
    did something smol

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
+            $table->string('name');
             $table->boolean('userRights')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
