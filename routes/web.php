5fa056df68c8600bc52c6879ee28be2057f165a0
Tommy Koning <99039195@mydavinci.nl>
2019-09-17 09:19:17 +0200

    Update .gitignore, Admin.php, and 2 more files...

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => '/admin', 'middleware' => ['adminCheck']], function(){
+    Route::get('home', 'HomeController@index')->name('home');
+});
+
+Route::get('home', 'HomeController@index')->name('home');
