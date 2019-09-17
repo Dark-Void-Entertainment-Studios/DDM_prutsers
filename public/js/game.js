e3cd610fef917c121a56ab761067822a9559fbce
kiran-kovacic <99048377@mydavinci.nl>
2019-09-17 14:16:45 +0200

    The option for obstacles is added to the board and can be turned on and off.

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,6 @@
 var game    = document.getElementById("game");
 var board   = [[13, 19], [26, 19]];
+var obstacles    = true;
 var button  = document.getElementById("button");
 
 /**
@@ -10,7 +11,7 @@ var button  = document.getElementById("button");
  * @param {*} a 
  */
 
-function makeBoard(a) {
+function makeBoard(a, random) {
     for (var rowCount = 0; rowCount < board[a][0]; rowCount++) {
         var row = document.createElement("div");
         row.setAttribute("class", "row");
@@ -22,15 +23,33 @@ function makeBoard(a) {
             square.style.width  = "50px";
             square.style.height = "50px";
             square.style.cssFloat = "right";
+            square.style.backgroundColor = "SaddleBrown ";
             row.appendChild(square);
         }
         game.appendChild(row);
+        if (obstacles) {
+            var b = randomSquare();
+                while (b == 0 || b == 18 ) {
+                    b = randomSquare();
+                }
+            var stone = document.getElementById("square_" + rowCount + "_" + b);
+            stone.style.backgroundImage = "url('img/stone.jpg')";
+            stone.style.backgroundSize  = "auto";
+            stone.setAttribute("onclick", "alert('You cannot place it here!')");
+        }
     }
 }
+
+function randomSquare() {
+    var random = Math.floor(Math.random() * 19);
+        return random;
+}
+
 function start() {
     makeBoard(0);
     button.style.display = "none";
 }
+
 (function init() {
     button.onclick = function(){start();};
 })();
