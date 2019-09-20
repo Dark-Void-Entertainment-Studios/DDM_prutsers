45bf90eb0da5e18f7c924f81c579108fac25ada6
herman <hermanmoret117@gmail.com>
2019-09-20 13:50:39 +0200

    nav
    Nav add a My account button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="/user">
+                                       My Account
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
