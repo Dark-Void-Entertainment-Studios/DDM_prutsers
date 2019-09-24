5f762134a96d3fbe69cdbd1f88e965f545597fe2
herman <hermanmoret117@gmail.com>
2019-09-24 22:10:01 +0200

    test
    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,5 +79,6 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('js/game.js') }}" defer></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/user', 'UserController@index');
+    Route::get('/board', 'GameController@index');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => ['adminCheck']], function(){
