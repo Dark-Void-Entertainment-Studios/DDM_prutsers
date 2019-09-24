853654951e5080494ef4036a00a7f468ab4c6a2b
Tommy Koning <99039195@mydavinci.nl>
2019-09-24 23:38:03 +0200

    updated user seeders before team mates could bitch i didnt make a user and unvertified user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -18,5 +18,18 @@ class UserTableSeeder extends Seeder
             'userRights' => 1,
             'email_verified_at' => Carbon\Carbon::now()
         ]);
+
+        DB::table('users')->insert([
+            'name' => "userTest",
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('wachtwoord'),
+            'email_verified_at' => Carbon\Carbon::now()
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "guestTest",
+            'email' => 'guest@gmail.com',
+            'password' => bcrypt('wachtwoord'),
+        ]);
     }
 }
