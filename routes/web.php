008b976d7ba707c2a07a0289bc4201fa38eaf4e4
rens gouman <99030922@mydavinci.nl>
2019-10-02 11:44:19 +0200

    tiny deletion
    deleted something that was commented

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,3 @@ Route::middleware(['auth'])->group(function () {
 Route::group(['prefix' => 'admin', 'middleware' => ['adminCheck']], function(){
     Route::get('/user', 'UserController@index');
 });
-
-/*
-Route::prefix('admin')->group(function () {
-    Route::get('users', function () {
-        // Matches The "/admin/users" URL
-    });
-});
