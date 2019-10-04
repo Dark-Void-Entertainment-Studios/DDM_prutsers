288d9e4708665e6d2df8edf539eb684d3a971adc
Tommy Koning <99039195@mydavinci.nl>
2019-10-04 09:13:24 +0200

    Update Monster.php

--- app/Monster.php
+++ app/Monster.php
@@ -11,14 +11,14 @@ class Monster
     private $hp;
     private $effect;
 
-    public function __construct($name, $level, $atk, $def, $hp, $effect)
+    public function __construct($name, $level, $atk, $def, $hp, $effect = "N/A")
     {
        $this->name = $name;
        $this->level = $level;
        $this->atk = $atk;
        $this->def = $def;
        $this->hp = $hp;
-       $this->effect $effect;
+       $this->effect = $effect;
     }
 
     public function attacked($dam) {
