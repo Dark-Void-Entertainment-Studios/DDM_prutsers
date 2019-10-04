655e42b56c773d3660f40fa1b1b616058b35eef0
Ian Storm <99039737@mydavinci.nl>
2019-10-04 14:46:52 +0200

    Making seeders, migrations and classes for all the characters. Also a model, controller and view.

---
+++ app/Characters/YumaTsukumo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Characters;
+
+use App\Characters;
+
+Class YumaTsukumo extends Characters
+{
+	public function __construct(){
+		parent::__construct('Yuma Tsukumo', 'W.I.P.');
+	}
+} 
