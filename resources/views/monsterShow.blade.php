6390ea7876e4055562853f6427d472dc557f9dff
tommy <tommy@LAPTOP-E7O1B38V.local>
2019-10-08 19:34:56 +0200

    Update monsterShow.blade.php

--- resources/views/monsterShow.blade.php
+++ resources/views/monsterShow.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{$monster->__get('name')}}</div>
                 <div class="card-body">
-                    <img class="image rounded" src="{{$monster->__get('imgPath')}}" alt="">
+                    <img class="image rounded" src="../{{$monster->__get('imgPath')}}" alt="">
                     <p>Level: {{$monster->__get('level')}}</p>
                     <p>Health points: {{$monster->__get('hp')}}</p>
                     <p>Attack: {{$monster->__get('atk')}}</p>
