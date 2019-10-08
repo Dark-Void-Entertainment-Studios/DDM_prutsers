f4f980e0bfad72d6bd7fee52b99c40368c810a39
tommy <tommy@LAPTOP-E7O1B38V.local>
2019-10-08 12:59:36 +0200

    Update DDM.php, aitsu.png, and aromajar.png

---
+++ app/Game/DDM.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Game;
+use Session;
+use Illuminate\Http\Request;
+
+use App\Monster;
+
+class DDM
+{
+    private $info = [];
+
+    public function __construct($request)
+    {
+        if($request->session()->has('ongoingGame')){
+            $this->info = $request->session()->get('ongoingGame');
+        } else {
+            $this->info = NULL;
+        }
+    }
+    public function startGame($request, $charactor, $player, $id)
+    {
+        if(empty($this->info[$id]))
+        {
+            $this->info[$id] = ['player' => $player, 'charactor' => $charactor];
+        } 
+        else
+        {
+            $this->info = NULL;
+        }
+        $this->saveGame($request);
+    }
+    private function saveGame($request)
+    {
+        $request->session()->put('info', $this->info);
+        $request->session()->save();
+    }
+}
