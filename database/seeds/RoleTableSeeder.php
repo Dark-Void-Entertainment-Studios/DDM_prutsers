9c5e06045619cb143ee2eccb3f6f6da8454f94dc
Tommy Koning <tommykoning@live.nl>
2019-11-19 23:26:05 +0100

    updated migrations and seeders

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'role' => "Basic"
+        ]);
+
+        DB::table('roles')->insert([
+            'role' => "Admin"
+        ]);
+
+        DB::table('roles')->insert([
+            'role' => "Super Admin"
+        ]);
+    }
+}
