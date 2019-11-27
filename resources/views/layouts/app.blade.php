d95fd6bb3526ed50c7a45c12036471c773cca248
kiran-kovacic <99048377@mydavinci.nl>
2019-11-27 12:07:10 +0100

    added ajax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/game.js') }}" defer></script>
     <script src="{{ asset('js/slide.js') }}" defer></script>
+    <script src="{{ asset('js/ajax.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
