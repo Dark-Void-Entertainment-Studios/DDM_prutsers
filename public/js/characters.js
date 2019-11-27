caf06d12c662aa34d6aebabc15acf44794f4bb7c
Ian Storm <99039737@mydavinci.nl>
2019-11-27 13:17:06 +0100

    selecting and changing characters for 2 persons

--- public/js/characters.js
+++ public/js/characters.js
@@ -1,13 +1,51 @@
-var idHealer = document.getElementById('select');
-var character = document.querySelector('input[name="character"]');
-
-document.getElementById(1).style.border = "2px solid lightblue";
+var p1 = document.getElementById('player1');
+var p2 = document.getElementById('player2');
+var player = 'player1';
+var s1 = document.getElementById('select1');
+var s2 = document.getElementById('select2');
 
 function saveCharacter(key) {
-	if (character.value != key) {
-		document.getElementById(character.value).style.border = "";
-	}
-  	document.getElementById(key).style.border = "2px solid lightblue";
-  	idHealer.value = key;
-  	character.value = key;
+  	if (player == 'player1') {
+  		if (document.getElementById(s1.value).classList.contains('player1')){
+    		document.getElementById(s1.value).classList.remove('player1');
+		} else if (document.getElementById(s1.value).classList.contains('playerBoth')){
+    		document.getElementById(s1.value).classList.remove('playerBoth');
+    		document.getElementById(s1.value).classList.add('player2');
+		}
+  		s1.value = key;
+  		if (document.getElementById(key).classList.contains('player2')){
+  			document.getElementById(key).classList.remove('player2');
+  			document.getElementById(key).classList.add('playerBoth');
+  		} else {
+  			document.getElementById(key).classList.add('player1');
+  		}
+  		p1.innerHTML = 'Player 1: ' + document.getElementById('name' + key).innerHTML;
+  		player = 'player2';
+  	} else if(player == 'player2') {
+  		if (document.getElementById(s2.value).classList.contains('player2')){
+    		document.getElementById(s2.value).classList.remove('player2');
+		} else if (document.getElementById(s2.value).classList.contains('playerBoth')){
+    		document.getElementById(s2.value).classList.remove('playerBoth');
+    		document.getElementById(s2.value).classList.add('player1');
+		}
+  		s2.value = key;
+  		if(document.getElementById(key).classList.contains('player1')){
+  			document.getElementById(key).classList.remove('player1');
+  			document.getElementById(key).classList.add('playerBoth');
+  		} else {
+  			document.getElementById(key).classList.add('player2');
+  		}
+  		p2.innerHTML = 'Player 2: ' + document.getElementById('name' + key).innerHTML;
+  		player = 'player1';
+  	}
 }
+
+function launch() {
+	document.forms['players'].submit();
+}
+
+(function(){
+	document.getElementById(1).classList.add('playerBoth');
+	p1.innerHTML = 'Player 1: ' + document.getElementById('name1').innerHTML;
+	p2.innerHTML = 'Player 2: ' + document.getElementById('name1').innerHTML;
+})();
--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -5,17 +5,18 @@
     <div class="d-flex justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <form action="/board" class="text-center">
-                <p class="characterTitle">You can select your character here!
-                <input class="btn btn-default bg-light" type="submit" value="Play"></p>
+            <form action="/board" class="text-center" name="players">
+            <div class="row"><div class="col-sm" id="player1">Player 1: </div><div class="characterTitle col-sm">You can select your character here!</div><div class="col-sm" id="player2">Player 2: </div></div>
                     @foreach($characters as $character)
-                <div class="imageContainer mw-50">
-                    <img src="{{$character->__get("imgPath")}}" class="image rounded" id="{{$loop->iteration}}" onclick="saveCharacter({{$loop->iteration}})">
-                    <div class="overlay">{{$character->__get('name')}}</div>
+                <div class="imageContainer mw-50" onclick="saveCharacter({{$loop->iteration}})">
+                    <img src="{{$character->__get("imgPath")}}" class="image rounded" id="{{$loop->iteration}}">
+                    <div class="overlay" id="name{{$loop->iteration}}">{{$character->__get('name')}}</div>
                 </div>
                     @endforeach
-                    <input type="hidden" name="character" value="1" id="select">
+                    <input type="hidden" name="character1" value="1" id="select1">
+                    <input type="hidden" name="character2" value="1" id="select2">
                 </form>
+                <input class="btn btn-default bg-light" onclick="launch()" value="Launch Game">
         </div>
     </div>
 </div>
