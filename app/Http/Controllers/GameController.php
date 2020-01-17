b1b67f759bf1a49b0e04092e834a7ddb1415705f
Tommy Koning <tommykoning@live.nl>
2020-01-17 20:07:52 +0100

    server sided board creation works (css is missing)

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\boardcreation\board;
 use Illuminate\Http\Request;
 use App\Game\DDM;
-use Session;
 use App\Personas;
 use App\monsters\Jinzo;
 
 
 class GameController extends Controller
 {
-    public $monster;
    /**
      * Show the application dashboard.
      *
@@ -20,13 +19,7 @@ class GameController extends Controller
     public function index(Request $request)
     {
         $DDM = new DDM($request);
-        $monster = new jinzo();
-        return view('playBoard');
-    }
-
-    public function getName()
-    {
-        $name = $monster->__get(name);
-        return $name;
+        $board = new board;
+        return view('playBoard', compact('board'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/game.js') }}" defer></script>
     <script src="{{ asset('js/slide.js') }}" defer></script>
     <script src="{{ asset('js/characters.js') }}" defer></script>
     <script src="{{ asset('js/ajax.js') }}" defer></script>
